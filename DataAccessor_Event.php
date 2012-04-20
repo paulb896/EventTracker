6a4fbedb004597733b408c019734e22472cafc6b
Paul Beauchamp <paulb896@gmail.com>
2012-04-19 20:58:37 -0600

    Added ability for dynamic database name and collection in the DataAccessor event class

--- DataAccessor_Event.php
+++ DataAccessor_Event.php
@@ -34,6 +34,7 @@ class DataAccessor_Event
      *    "userEmail" => "ftw@hotmail.com",
      *    "Location" => "-33,151", //Google api input format Longitude/Latatude,
      *   )
+     * 
      * @return array Array of single event information arrays.
      */
     public function getEvents(array $eventConstraints)
@@ -42,12 +43,25 @@ class DataAccessor_Event
     }
 
     /**
-     * Mongo collection name.
+     * Name of mongo database.
+     * @var string
+     */
+    protected $_databaseName = 'myNewDatabase';
+
+    /**
+     * Name of mongo database.
+     * @var string
+     */
+    protected $_collectionName = 'events';
+
+    /**
+     * Instance of mongo collection.
      * @var string
      */
     protected $_collection;
 
     /**
+     * Instance of mongo database.
      * @var MongoDB instance
      */
     protected $_db;
@@ -55,14 +69,12 @@ class DataAccessor_Event
     /**
      * Instantiate new instance of mongo database
      * and set class collection to mongoDB events collection.
-     *
-     * todo: Add dynamic collection and database names.
      */
     protected function _constructDb()
     {
-        $mongoDatabase = new Mongo();
-        $this->_db = $mongoDatabase->myNewDatabase;
-        $this->_collection = $this->_db->events;
+        $mongoConnection = new Mongo();
+        $this->_db = $mongoConnection->selectDB($this->_databaseName);
+        $this->_collection = new MongoCollection($this->_db, $this->_collectionName);
     }
 }
 ?>
