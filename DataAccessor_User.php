9909592d568312a39bb1da8e2fe32acccd32628e
Paul Beauchamp <paulb896@gmail.com>
2012-04-22 21:17:30 -0600

    Cleaned up tests and added user mongo database data accessor

---
+++ DataAccessor_User.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Require extended class.
+ */
+ require_once 'DataAccessor_AbstractMongo.php';
+
+/**
+ * MongoDB relations for users collection.
+ */
+class DataAccessor_User extends DataAccessor_AbstractMongo
+{
+    /**
+     * Get name of this collection.
+     * 
+     * @return string Name of collection.
+     */
+    protected function _getCollectionName()
+    {
+        return 'users';
+    }
+
+    /**
+     * Verify data is acceptable for collection.
+     * 
+     * @param array Array of data keyed by data name.
+     * @return bool True if data is valid for collection.
+     */
+    protected function _isValidCollectionData(array $data)
+    {
+        $validData = array(
+            'userEmail' => 'string',
+            'alias' => 'string'
+        );
+
+        if (count(array_diff_key($validData, $data)) !== 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
+?>
--- test/DataAccessor_EventTest.php
+++ test/DataAccessor_EventTest.php
@@ -1,58 +1,111 @@
 <?php
 
-echo "\n Now running tests to exercise event class\n\n";
+
 
 // Setup test data.
 
-$testEventDataExample1 = array(
-    'dateTime' => '2012-12-24 18:56:35',
-    'timestamp' => date("Y-m-d H:i:s"),
-    'shortDescription' => 'End of civilization as we currently know it',
-    'location' => '-33.8670522,151.1957362',
-    'userEmail' => 'fake@fake.ca'
-);
 
-$testEventDataExample2 = array(
-    'dateTime' => '1995-05-23 11:00:00',
-    'timestamp' => date("Y-m-d H:i:s"),
-    'shortDescription' => 'I really ',
-    'location' => '-33.8670522,151.1957362',
-    'userEmail' => 'fake2@fake.ca'
-);
 
-$testDBName = 'myTestDatabase942';
 
-require_once '../DataAccessor_Event.php';
-$event = new DataAccessor_Event();
 
-// Set event object database to test database.
-$event->databaseName = $testDBName;
 
-// Setup test mongo database.
-$mongoConnection = new Mongo();
-$testDB = $mongoConnection->selectDB($testDBName);
-$collection = new MongoCollection($testDB, 'events');
 
-// Test to save event.
-print "\nRunning save event test.";
-$event->insertIntoCollection($testEventDataExample1);
-$dataFromTestDB = $collection->findOne();
-if ($testEventDataExample1['dateTime'] != $dataFromTestDB['dateTime']) {
-    print "\n\n ! An error has occurred while attempting to save an event.\n\n";
+
+
+
+
+
+class DataAccessor_EventTest
+{
+    /**
+     * Setup test data and test mongo database.
+     */
+    public function __construct()
+    {
+        $this->_testData['event1'] = array(
+            'dateTime' => '2012-12-24 18:56:35',
+            'timestamp' => date("Y-m-d H:i:s"),
+            'shortDescription' => 'End of civilization as we currently know it',
+            'location' => '-33.8670522,151.1957362',
+            'userEmail' => 'fake@fake.ca'
+        );
+
+        $this->_testData['event2'] = array(
+            'dateTime' => '1995-05-23 11:00:00',
+            'timestamp' => date("Y-m-d H:i:s"),
+            'shortDescription' => 'I really ',
+            'location' => '-33.8670522,151.1957362',
+            'userEmail' => 'fake2@fake.ca'
+        );
+
+        $mongoConnection = new Mongo();
+        $this->_testDatabase = $mongoConnection->selectDB($this->_testDatabaseName);
+    }
+
+    /**
+     * Drop test mongo database.
+     */
+    public function __destruct()
+    {
+        // Delete data from test database.
+        $this->_testDatabase->drop();
+    }
+
+    /**
+     * Verify save functionality.
+     */
+    public function testSave()
+    {
+        print "\nRunning save event test.";
+
+        require_once '../DataAccessor_Event.php';
+        $event = new DataAccessor_Event();
+        // Set event object database to test database.
+        $event->databaseName = $this->_testDatabaseName;
+
+        $event->insertIntoCollection($this->_testData['event1']);
+
+        $collection = new MongoCollection($this->_testDatabase, 'events');
+        $dataFromTestDB = $collection->findOne();
+
+        if ($this->_testData['event1']['dateTime'] != $dataFromTestDB['dateTime']) {
+            print "\n\n ! An error has occurred while attempting to save an event.\n\n";
+        }
+    }
+
+    /**
+     * Verify find event functionality.
+     */
+    public function testFind()
+    {
+        //print "\nRunning get events test.";
+        //$allEventsForUser1 = $event->getAll(array('userEmail' => $testEventDataExample1['userEmail']));
+        //if (count($allEventsForUser1) !== 1) {
+            
+        //};
+    }
+
+    /**
+     * Array of input test data keyed by data name.
+     */
+    protected $_testData;
+
+    /**
+     * Name of mongo database we will use for testing.
+     * @var string
+     */
+    protected $_testDatabaseName = 'myTestDatabase942';
+
+    /**
+     * Instance of mongo database connection.
+     * @var MongoDB
+     */
+    protected $_testDatabase;
 }
 
-// Save an extra record to verify that the query works properly.
-$event->insertIntoCollection($testEventDataExample2);
-
-// Running test to get events.
-//print "\nRunning get events test.";
-//$allEventsForUser1 = $event->getAll(array('userEmail' => $testEventDataExample1['userEmail']));
-//if (count($allEventsForUser1) !== 1) {
-	
-//};
-
-// Delete data from test database.
-$testDB->drop();
+echo "\n Now running tests to exercise event class\n\n";
+$eventTestClass = new DataAccessor_EventTest();
+$eventTestClass->testSave();
 
 print "\nTesting has finished!\n\n";
 ?>
